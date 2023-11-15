0126c0b3d4a04c523f0cdf3329945809831f9a43
choi.jihee <choi_jihee@radcns.com>
2023-11-15 14:35:10 +0900

    일기쓰기

--- 개인/emotion-diary/src/components/DiaryItem.js
+++ 개인/emotion-diary/src/components/DiaryItem.js
@@ -3,12 +3,32 @@ import MyButton from './MyButton';
 
 const DiaryItem = ({id, emotion, content, date}) => {
     const navigate = useNavigate();
+    const env = process.env;
+    env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+    const strDate = new Date(parseInt(date)).toLocaleDateString()
+
+    // 클릭한 해당 아이템 상세 페이지로 이동해야하니까
+    const goDetail = () => {
+        navigate(`/diary/${id}`)
+    }
+
+    const goEdit = () => {
+        navigate(`/edit/${id}`)
+    }
+
     return (
         <div className='DiaryItem'>
-            <img src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`} alt=''/>
-            <p>{new Date(date).toLocaleDateString()}</p>
-            <p>{content}</p>
-            <MyButton type={'default'} text={'수정하기'} onClick={()=>{navigate('/Edit')}}/>
+            <div className={['emotion_img_wrapper', `emotion_img_wrapper_${emotion}`].join(" ")} onClick={goDetail}>
+                <img src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`} alt=''/>
+            </div>
+            <div className='info_wrapper' onClick={goDetail}>
+                <p className='diary_date'>{strDate}</p>
+                <p className='diary_content_preview'>{content.slice(0,25)}</p>
+            </div>
+            <div className='btn_wrapper'>
+               <MyButton type={'default'} text={'수정하기'} onClick={goEdit}/>
+            </div>
         </div>
     )
 }

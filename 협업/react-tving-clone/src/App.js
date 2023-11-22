dccd728510d6b136abb516cbc4ae1264ba85f74c
choi.jihee <choi_jihee@radcns.com>
2023-11-22 09:58:19 +0900

    테스트

--- 협업/react-tving-clone/src/App.js
+++ 협업/react-tving-clone/src/App.js
@@ -1,9 +1,19 @@
+import {useEffect, useState} from 'react';
+import Slide from './component/Slide';
 import './assets/css/reset.scss'
 
+import {itemList} from './utils/itemList';
+
 function App(){
+  const [moveList, setMoveList] = useState([]);
+  
+  useEffect(()=>{
+    setMoveList(itemList);
+  },[])
+
   return(
     <div className='App'>
-      
+      <Slide moveList={moveList}/>
     </div>
   )
 }
